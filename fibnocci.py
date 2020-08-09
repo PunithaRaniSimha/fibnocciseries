4863163d2362bde6422486d8c0760f1d710e2658
PunithaRaniSimha <68122889+PunithaRaniSimha@users.noreply.github.com>
2020-08-09 21:51:13 +0530

    created fibnocci.py
    This file prints the output in fib series.

---
+++ fibnocci.py
@@ -0,0 +1,18 @@
+
+def fn(number):
+    a = 0
+    b = 1
+    if number == 0:
+        print(a)
+    else:
+        print(a)
+        print(b)
+        for i in range(2,number):
+            c = a+b
+            a = b
+            b = c
+            print(c)
+fn(13)        
+        
+            
+
